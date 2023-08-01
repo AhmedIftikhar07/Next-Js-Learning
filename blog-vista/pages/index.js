3b35a9cbe8f029b90c729c9c4ea36fd808e5bc19
AhmedIftikhar07 <sheikhchilli401@gmail.com>
2023-08-01 13:03:57 -0700

    initial styling

--- blog-vista/pages/index.js
+++ blog-vista/pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -15,95 +15,45 @@ export default function Home() {
         <meta name="keywords" content="nextjx, blog vista, hunting coder"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <body className={styles.body}>
+        
+      
+      {/* <Script src='' strategy='lazyOnLoad'></Script> */}
+      <nav className={styles.mainNav}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>blog</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
 
-        <div className={styles.center}>
+      <main className={`${styles.main}`}>
+        
+
+        <div className={styles.main}>
           <h1>Blog Vista</h1>
+          <p>A blog for hunting coders by blog vista</p>
         </div>
 
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className="blogs">
+          <h2>Popular Blogs</h2>
+        <div className="blogItem">
+          <h3>How to learn JavaScript in 2023?</h3>
+          <p>JavaScript is the language used to design logics for the web</p>
+        </div>
+        <div className="blogItem">
+          <h3>How to learn JavaScript in 2023?</h3>
+          <p>JavaScript is the language used to design logics for the web</p>
+        </div>
+        <div className="blogItem">
+          <h3>How to learn JavaScript in 2023?</h3>
+          <p>JavaScript is the language used to design logics for the web</p>
+        </div>
         </div>
       </main>
+      </body>
     </>
   )
 }
