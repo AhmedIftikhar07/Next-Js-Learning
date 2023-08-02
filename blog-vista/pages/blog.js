35e5377650e56cc52287a5626cb40e0a737c77f0
AhmedIftikhar07 <sheikhchilli401@gmail.com>
2023-08-02 12:56:00 -0700

    blogPost

--- blog-vista/pages/blog.js
+++ blog-vista/pages/blog.js
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import styles from '../styles/blog.module.css'
+import Link from 'next/link'
 const blog = () => {
   return (
     <div>
-      this is blog page
+      <main className={`${styles.main}`}>
+      <div>
+            <div className="blogItem">
+              <Link href='/blogpost/learn-javascript'>
+              <h3>How to learn JavaScript in 2023?</h3>
+              </Link>
+              <p>JavaScript is the language used to design logics for the web</p>
+            </div>
+            <div className="blogItem">
+              <h3>How to learn JavaScript in 2023?</h3>
+              <p>JavaScript is the language used to design logics for the web</p>
+            </div>
+            <div className="blogItem">
+              <h3>How to learn JavaScript in 2023?</h3>
+              <p>JavaScript is the language used to design logics for the web</p>
+            </div>
+          </div>
+          </main>
     </div>
   )
 }
