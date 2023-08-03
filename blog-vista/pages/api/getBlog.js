854735488036658b0bba58e75a9ab02d1affc4ff
AhmedIftikhar07 <sheikhchilli401@gmail.com>
2023-08-03 02:53:32 -0700

    Making endPoints

---
+++ blog-vista/pages/api/getBlog.js
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// http://localhost:3000/api/getBlog?slug=how-to-learn-javascript
+import * as fs from 'fs'
+export default function handler(req, res) {
+ fs.readFile(`blogdata/${req.query.slug}.json`,'utf-8',(err , data)=>{
+  if(err){
+    res.status(500).json({error: "some error occur"})
+  }
+   res.status(200).json(JSON.parse(data))
+})
+}
--- blog-vista/pages/blogpost/[slug].js
+++ blog-vista/pages/blogpost/[slug].js
@@ -2,9 +2,11 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import styles from '../../styles/blogPost.module.css'
 
+
 const Slug = () => {
     const router = useRouter()
     const {slug} = router.query;
+
   return (
     <div className={styles.container}>
       
@@ -12,27 +14,15 @@ const Slug = () => {
         <h1>{slug}</h1>
       </header>
       <main className={styles.main}>
-       
+
         <div className={styles.aboutContent}>
-          <p>
-            At Blog Vista Coder, we are dedicated to providing you with valuable insights, practical tips, and the latest trends in the world of coding. Whether you're a seasoned developer, an aspiring coder, or simply someone curious about the fascinating world of programming, this blog is your go-to destination.
 
-            Our team of passionate coders and tech enthusiasts is committed to delivering high-quality content that covers a wide range of programming languages, frameworks, tools, and best practices. From web development to mobile apps, machine learning to data science, we've got you covered.
-
-            Through our articles, tutorials, and hands-on guides, we aim to simplify complex concepts, making coding accessible to everyone. Whether you're looking to sharpen your coding skills, stay updated with industry news, or explore new technologies, Blog Vista Coder is your reliable companion on this exciting journey.
-
-            We believe in the power of sharing knowledge and fostering a vibrant community of coders. Feel free to engage with us through comments, discussions, and sharing your own experiences. Together, we can learn, grow, and build innovative solutions that shape the future of technology.
-
-            Join us on this coding adventure as we unravel the mysteries of programming and explore the endless possibilities it offers. Whether you're a developer looking to level up your skills or someone curious about the magic behind the apps and websites you use daily, Blog Vista Coder is here to inspire and empower you.
-
-            Thank you for being a part of our community. Let's embark on this exciting coding journey together! Happy coding!
-          </p>
-          
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores ipsa velit nesciunt consequatur animi corporis iure voluptatem recusandae sequi omnis facilis ad autem, porro hic nihil, quos nam. Quis, libero?</p>            
         </div>
       </main>
       
     </div>
-  )
-}
+  
+)}
 
 export default Slug
